7a085097091e3caf2991239534cc14697c822dd1
trupatel <137491637+trupatel@users.noreply.github.com>
2026-02-06 12:51:23 -0700

    Add files via upload

---
+++ RSO_pack/ravens_simplier_code/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 12:42:57 2026
+
+@author: maste
+"""
+
+if __package__ in [None, '']:
+    from wire2PL import  wire2PL
+else:
+    from .wire2PL import  wire2PL
